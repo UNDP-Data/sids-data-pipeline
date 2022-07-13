c6f9a93e731cd1695655ad81d76248407cbd1e1e
Max Malynowsky <github@maxmalynowsky.com>
2022-07-13 15:43:32 -0400

    Add capability to ignore already processed tiles

--- batch/deploy.sh
+++ batch/deploy.sh
@@ -1,3 +1,5 @@
+envsubst <manifest.tpl.yml >manifest.yml
 docker container prune -f && docker image prune -af && docker compose build
 docker push undpgeohub.azurecr.io/sids-data-batch
+az container delete --resource-group undpdpbppssdganalyticsgeo --name sids-data-batch --yes
 az container create --resource-group undpdpbppssdganalyticsgeo --file deploy.yml
