172e26c675c7ac853ac08c9041a18ab6b55a0883
Max Malynowsky <github@maxmalynowsky.com>
2022-07-27 20:58:13 -0400

    fix cleanup script

--- pipeline/processing/cleanup.py
+++ pipeline/processing/cleanup.py
@@ -4,11 +4,7 @@ from .utils import cwd, logging
 
 logger = logging.getLogger(__name__)
 query_1 = """
-    DROP TABLE IF EXISTS {table_in1};
-    DROP TABLE IF EXISTS {table_in2};
-"""
-query_2 = """
-    DROP TABLE IF EXISTS {table_in1};
+    DROP TABLE IF EXISTS {table_in};
 """
 
 
@@ -29,7 +25,7 @@ def clean_tmp_vector(r_row, vector_data, *_):
 def clean_db_input(row, _, __, cur):
     r_id = row['id'].lower()
     cur.execute(SQL(query_1).format(
-        table_in1=Identifier(r_id),
+        table_in=Identifier(r_id),
     ))
 
 
@@ -38,14 +34,14 @@ def clean_db_raster(r_row, vector_data, _, cur):
         v_id = v_row['id']
         r_id = r_row['id'].lower()
         cur.execute(SQL(query_1).format(
-            table_in2=Identifier(f'{v_id}_{r_id}'),
+            table_in=Identifier(f'{v_id}_{r_id}'),
         ))
 
 
 def clean_db_vector(row, _, __, cur):
     v_id = row['id']
-    cur.execute(SQL(query_2).format(
-        table_in1=Identifier(v_id),
+    cur.execute(SQL(query_1).format(
+        table_in=Identifier(v_id),
     ))
 
 
